e6bf16165a1aac822207a33518b734aeeb502b02
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2022-05-13 10:18:38 -0300

    using user event

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { SummaryForm } from '../SummaryForm';
 
 test('checkbox is unchecked by default', () => {
@@ -11,17 +12,25 @@ test('checkbox is unchecked by default', () => {
   expect(checkbox).not.toBeChecked();
 });
 
-test('checking/unchecking checkbox enables/disables button', () => {
+test('checking/unchecking checkbox enables/disables button', async () => {
   render(<SummaryForm />);
+
+  const user = userEvent.setup();
 
   const checkbox = screen.getByRole('checkbox', {
     name: /i agree to terms and conditions/i
   });
   const button = screen.getByRole('button', { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await act(async () => {
+    await user.click(checkbox);
+  });
   expect(button).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await act(async () => {
+    await user.click(checkbox);
+  });
   expect(button).toBeDisabled();
 });
+
+test.todo('popover responds to hover');
